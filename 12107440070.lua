df9cc7d6560986c4b2fe200c8231e5ab99a7d5ad
Cosmic <71171666+cosmicpsx@users.noreply.github.com>
2023-09-17 17:41:55 +0200

    Update 12107440070.lua

--- 12107440070.lua
+++ 12107440070.lua
@@ -1,1 +1,4 @@
-print()
+firetouchinterest(game.Workspace.TP['TP10']['TP10'], game.Players.LocalPlayer.Character.PrimaryPart, 0)
+queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/cosmicpsx/luna/main/12107440070.lua"))
+wait(1)
+game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
