ac29ffd61b95a34fb946deb3c2274c93fd9a2caa
Cosmic <71171666+cosmicpsx@users.noreply.github.com>
2024-01-12 16:49:48 +0100

    Delete 12107440070.lua

--- 12107440070.lua
+++
@@ -1,6 +0,0 @@
-wait(5)
-firetouchinterest(game.Workspace.TP['TP10']['TP10'], game.Players.LocalPlayer.Character.PrimaryPart, 0)
-queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/cosmicpsx/luna/main/12107440070.lua"))
-wait(1)
-game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
-
